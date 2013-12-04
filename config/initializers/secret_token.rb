ebaec47fe93ae603ee951815d9a05c7ef4879d39
LesleyMdCampo <lesliz32@gmail.com>
2013-12-04 15:11:57 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YelperSquare::Application.config.secret_key_base = '32f3c68e925fee0902374de3a965d538da1d4eadc29d2a49ccd2f0c8d5d8f156430ff7762c0d1c2c1e7ed9e4082a4f1de8dcde0f184b3999ddd1cfde8def5bd0'
