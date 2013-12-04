fde7df77697b344d188e108cc48307d12668e7cd
LesleyMdCampo <lesliz32@gmail.com>
2013-12-04 15:29:55 -0600

    Add controller methods for CRUD.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,40 @@
 class RestaurantsController < ApplicationController
+	def show
+		@restaurant = Restuarant.find(params[:id])
+	end
 
+	def index
+		@restaurants = Restuarant.all
+	end
+
+	def new
+		@restaurant = Restuarant.new
+	end
+
+	def create
+		@restaurant = Restuarant.new(restuarant_params)
+		@restaurant.save
+		redirect_to root_path
+	end
+
+	def edit
+		@restaurant = Restuarant.find(params[:id])
+	end
+
+	def update
+		@restaurant = Restuarant.find(params[:id])
+		@restaurant.update(restuarant_params)
+		redirect_to root_path
+	end
+
+	def destroy
+		@restaurant = Restuarant.find(params[:id])
+		@restaurant.destroy
+		redirect_to root_path
+	end
+	
+	private
+	def location_params
+		params.require( ).permit()
+	end
 end
